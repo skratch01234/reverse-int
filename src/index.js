1b8f3bbb323135c8036320e17b59b50fe4ead489
skratch01234 <skratch1234@rambler.ru>
2022-06-26 00:46:10 +0300

    feat:result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    
+    if(n<0){
+     let minus=String(n).split('')
+     let plus=minus.slice(1)
+     plus=plus.reverse().join('')
+     return +plus
+    }else{
+        let rev=String(n).split('').reverse().join('') 
+        return +rev
+    }
+
+    
+
 }
